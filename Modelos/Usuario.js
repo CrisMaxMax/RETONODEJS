506f6442b968af95ba7a5cb97f0ffd06f0ab540e
CrisMaxMax <cristofher.suarezca@amigo.edu.co>
2021-11-03 14:47:38 -0500

    completar tareas CRUD

--- Modelos/Usuario.js
+++ Modelos/Usuario.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 let UsuarioSchema = new mongoose.Schema({
+    typeDocument : String,
     userId: Number,
     userName: String,
+    userLastName : String,
+    userAddress : String,
+    userMail : String,
+    phoneUser : Number,
+    descripcion : String,
     celUser: Number,
+    sitioweb : String,
     cityUser: String
 });
 
--- server.js
+++ server.js
@@ -31,11 +31,19 @@ router.get('/Usuario', (req, res) =>{
 // Insertar
 router.post('/Usuario', (req, res) =>{
     let nuevoUsuario = new UsuarioSchema({
+        typeDocument : req.body.tipoDocumento,
         userId: req.body.id,
         userName: req.body.nombre,
-        celUser: req.body.telefono,
-        cityUser: req.body.ciudad
+        userLastName : req.body.apellido,        
+        userAddress : req.body.direccion,
+        userMail : req.body.mail,
+        phoneUser : req.body.telefono,
+        descripcion : req.body.descri,
+        celUser: req.body.cel,
+        sitioweb : req.body.web,
+        cityUser: req.body.ciudad,    
     });
+
     nuevoUsuario.save(function(err, datos){
         if(err){
             console.log(err);
@@ -43,6 +51,34 @@ router.post('/Usuario', (req, res) =>{
         res.send("Usuario Almacenado")
     })
 })
+
+router.delete('/Usuario', (req,res)=>{
+    UsuarioSchema.findById(userId,(err)=>{
+        if(err){
+            console.log("error buscando usuario")
+        }
+        UsuarioSchema.remove(err =>{
+            if (err){
+                console.log("error borrando usuario")
+            }else{
+                res.send("Usuario borrado")
+            }
+        })
+    })
+});
+
+router.put('/Usuario', (req,res)=>{
+    let nuevoUsuario = req.params.nuevoUsuario
+    let update = req.body
+    UsuarioSchema.findByIdAndUpdate(userId, update, (err,nuevoUsuarioUpdated )=>{
+        if(err){
+            console.log("error actualizar")
+
+        }else{
+            res.send("usuario actualizado")
+        }
+    })
+});
 
 app.use(router);
 app.listen(3000, ()=> {
